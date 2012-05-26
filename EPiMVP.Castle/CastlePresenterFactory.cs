63f727e9679fac65f4fd9fe446e5180ebd4215fa
Mike Hook <mike.hook@fortunecookie.co.uk>
2012-05-26 11:34:51 +0100

    Re-added EPiMVP.Castle, EPiMVP.Ninject and EPiMVP.StructureMap (After detached head balls up!)

---
+++ EPiMVP.Castle/CastlePresenterFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Castle.MicroKernel;
+using PageTypeBuilder;
+using WebFormsMvp;
+
+namespace EPiMVP.Castle
+{
+	public class CastlePresenterFactory : EPiPresenterFactory
+	{
+		private readonly IKernel presenterKernel;
+
+		public CastlePresenterFactory(IKernel kernel)
+		{
+			if (kernel == null)
+				throw new ArgumentNullException("kernel");
+			this.presenterKernel = kernel;
+		}
+
+		protected override bool CanUseConstructor(ConstructorInfo constructor, Type viewType, Type pageDataType)
+		{
+			ParameterInfo[] parameters = constructor.GetParameters();
+			if (parameters[0].ParameterType.IsAssignableFrom(viewType))
+				return parameters[1].ParameterType.IsAssignableFrom(pageDataType);
+			else
+				return false;
+		}
+
+		protected override IPresenter CreatePresenterInstance(Type presenterType, TypedPageData pageData, Type viewType, IEPiView view)
+		{
+			if (presenterType == (Type)null)
+				throw new ArgumentNullException("presenterType");
+			if (viewType == (Type)null)
+				throw new ArgumentNullException("viewType");
+			if (view == null)
+				throw new ArgumentNullException("view");
+			Dictionary<string, object> dictionary = new Dictionary<string, object>()
+      {
+        {
+          "view",
+          (object) view
+        },
+        {
+          "pageData",
+          (object) pageData
+        }
+      };
+			return (IPresenter)this.presenterKernel.Resolve(presenterType, (IDictionary)dictionary);
+		}
+	}
+}
